4dc44c68b371625fbbc40e32ae2e29395a03bd3e
Jevin Sweval <jevinsweval@gmail.com>
2018-08-20 14:46:36 -0400

    srec-dump-tester build for linux

--- srec-dump-tester.c
+++ srec-dump-tester.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #include "srec-dump.h"
 
-#ifdef __APPLE__
-extern void *__dso_handle;
+#if defined(__APPLE__)
+extern uint8_t __dso_handle;
 #define EXE_START (&__dso_handle)
+#elif defined(__unix__)
+extern uint8_t __executable_start;
+#define EXE_START (&__executable_start)
 #endif
 
 int main(int argc, const char **argv) {
